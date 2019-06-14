686b92c798566f45968bb1ac83bf8490697119f9
darshan.mistry <darshan.mistry@softwebsolutions.com>
2019-06-14 19:14:32 +0530

    Add tricky program of Java

---
+++ src/ReverseString.java
@@ -0,0 +1,12 @@
+public class ReverseString {
+
+    public static void main(String[] args) {
+        StringBuffer sbf = new StringBuffer("MyJava");
+
+        System.out.println(sbf.reverse());    //
+
+        StringBuilder stringBuilder = new StringBuilder("Darshan");
+
+        System.out.println(stringBuilder.reverse());    //
+    }
+}
