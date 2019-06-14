10a20d3fb55560adcaf859da6a0aa00e59701d82
darshan.mistry <darshan.mistry@softwebsolutions.com>
2019-06-14 14:06:54 +0530

    sub array sum

--- src/SubArraySum.java
+++ src/SubArraySum.java
@@ -5,7 +5,8 @@ public class SubArraySum {
     public static void main(String[] args) {
 
 
-        findSubArray(new int[]{42, 15, 12, 8, 6, 32}, 26);
+//        findSubArray(new int[]{42, 15, 12, 8, 6, 32}, 26);
+        subArraySum(new int[]{42, 15, 12, 8, 6, 32}, 26);
     }
 
     static void findSubArray(int[] inputArray, int inputNumber) {
@@ -48,5 +49,37 @@ public class SubArraySum {
         }
 
         System.out.println("=================================");
+    }
+
+    /* Returns true if the there is a subarray of arr[] with sum equal to
+       'sum' otherwise returns false.  Also, prints the result */
+    static int subArraySum(int arr[], int sum) {
+        int n = arr.length;
+        int curr_sum = arr[0], start = 0, i;
+
+        // Pick a starting point
+        for (i = 1; i <= n; i++) {
+            // If curr_sum exceeds the sum, then remove the starting elements
+            while (curr_sum > sum && start < i - 1) {
+                curr_sum = curr_sum - arr[start];
+                start++;
+            }
+
+            // If curr_sum becomes equal to sum, then return true
+            if (curr_sum == sum) {
+                int p = i - 1;
+                System.out.println("Sum found between indexes " + start
+                        + " and " + p);
+                return 1;
+            }
+
+            // Add this element to curr_sum
+            if (i < n)
+                curr_sum = curr_sum + arr[i];
+
+        }
+
+        System.out.println("No subarray found");
+        return 0;
     }
 }
