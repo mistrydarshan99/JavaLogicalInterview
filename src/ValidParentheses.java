6335e02dc08afe0bb66ef991c8a378cf58ccaafc
darshan.mistry <darshan.mistry@softwebsolutions.com>
2019-07-19 16:58:09 +0530

    Add tricky program of Java

---
+++ src/ValidParentheses.java
@@ -0,0 +1,24 @@
+public class ValidParentheses {
+
+    public static void main(String[] args) {
+//        System.out.println(isValid("(){}[]"));
+        System.out.println(isValid("{[]}"));
+    }
+
+    private static boolean isValid(String s) {
+        char[] stack = new char[s.length() + 1];
+        int top = 1;
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack[top++] = c;
+            } else if (c == ')' && stack[--top] != '(') {
+                return false;
+            } else if (c == ']' && stack[--top] != '[') {
+                return false;
+            } else if (c == '}' && stack[--top] != '{') {
+                return false;
+            }
+        }
+        return top == 1;
+    }
+}
