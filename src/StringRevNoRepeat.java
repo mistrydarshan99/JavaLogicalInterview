73c2711e5cb7c89959c716bdc8696378a5778884
darshan.mistry <darshan.mistry@softwebsolutions.com>
2019-07-24 12:27:00 +0530

    Add tricky program of Java

---
+++ src/StringRevNoRepeat.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+
+public class StringRevNoRepeat {
+
+    public static void main(String[] args) {
+        seconTrickRev("abbac"); //output: caba
+        seconTrickRev("abbbaa"); //output: aba
+    }
+
+    private static String stringRev(String str) {
+        char[] chars = str.toCharArray();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = chars.length - 1; i >= 0; i--) {
+            if (i > 0 && i < chars.length - 1 && chars[i] != chars[i + 1]) {
+                stringBuilder.append(chars[i]);
+            } else if (i == chars.length - 1 || i == 0) {
+                stringBuilder.append(chars[i]);
+            }
+        }
+        System.out.println(stringBuilder.toString());
+        return "";
+    }
+
+    private static String seconTrickRev(String str) {
+        char[] chars = str.toCharArray();
+        StringBuilder stringBuilder = new StringBuilder();
+        char nextChar = '\0';
+        for (int i = chars.length - 1; i >= 0; i--) {
+            if (nextChar != chars[i]) {
+                nextChar = chars[i];
+                stringBuilder.append(chars[i]);
+            }
+        }
+        System.out.println(stringBuilder.toString());
+        return "";
+    }
+}
